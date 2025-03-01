3520b311c769c2583f89c43dd3da79e1ef089e75
atin5 <atin5@naver.com>
2025-03-01 21:44:42 +0900

    app.py

--- app.py
+++ app.py
@@ -43,8 +43,6 @@ def save_all_companies(companies, file_path):
             ])
             f.write(line + "\n")
 
-st.title("Marry U")
-
 # 관리자 모드 활성화 (패스워드: admin123)
 admin_mode = st.sidebar.checkbox("관리자 모드 활성화")
 if admin_mode:
